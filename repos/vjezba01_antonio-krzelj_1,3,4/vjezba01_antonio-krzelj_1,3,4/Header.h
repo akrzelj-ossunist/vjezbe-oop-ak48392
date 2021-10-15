69dcba3a6f89de1f94308562ce9b213fa7f0a9fb
akrzelj-ossunist <92447214+akrzelj-ossunist@users.noreply.github.com>
2021-10-15 14:30:21 +0200

    Add files via upload

---
+++ repos/vjezba01_antonio-krzelj_1,3,4/vjezba01_antonio-krzelj_1,3,4/Header.h
@@ -0,0 +1,7 @@
+#ifndef HEADER_H
+#define HEADER_H
+
+void funk();
+void niz(int* i, int* j);
+int rek(int* niz, int max, int min, int i, int* j, int* k);
+#endif#pragma once
---
+++ repos/vjezba01_antonio-krzelj_1,3,4/vjezba01_antonio-krzelj_1,3,4/rekurzija.cpp
@@ -0,0 +1,21 @@
+﻿/*4. Napisati rekurzivnu funkciju za traženje najvećeg i najmanjeg broja u nizu.*/
+
+
+#include "Header.h"
+#include <iostream>
+#include <iomanip>
+int rek(int* niz, int max, int min, int i, int* j, int* k)
+{
+	if (i < 0)
+	{
+		*j = max;
+		*k = min;
+		return 0;
+	}
+	if (max < niz[i])
+		max = niz[i];
+	if (min > niz[i])
+		min = niz[i];
+	i--;
+	return rek(niz, max, min, i, j, k);
+}
